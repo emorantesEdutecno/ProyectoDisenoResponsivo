ef2f155da01cc1c38f78a62a1ca7a6487e09bd62
Elfar Morantes <emorantes@edutecno.com>
2023-04-25 21:58:31 -0500

    crear colapsables de menu y chats

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -39,3 +39,42 @@ let cambiarFuenteOrbitron= ()=>{
     elBoton[0].disabled = false;
 
 }
+
+
+
+let mostrarContenedoresChat= ()=>{
+    let losChats = document.getElementsByClassName('principal__izquierda__contenedortextoIzquierda');
+    let estado = losChats[0].style.display;
+
+    if(estado == 'inherit'){
+        for(let i=0; i < losChats.length; i++){
+            losChats[i].style.display = 'none';
+        }
+    }
+    else{
+        for(let i=0; i < losChats.length; i++){
+            losChats[i].style.display = 'inherit';
+        }
+    }
+
+}
+
+
+
+let mostrarMenuPagina= ()=>{
+
+    let elContenedor = document.getElementsByClassName('cabecera__contenedorLista');
+    let estadoContenedor = elContenedor[0].style.display;
+
+    let elBotonChats = document.getElementsByClassName('principal__izquierda__mostrarChats__boton');
+    if(estadoContenedor == 'block'){
+        elContenedor[0].style.display = 'none';
+        elBotonChats[0].style.top = '30px';
+    }
+    else{
+        elContenedor[0].style.display = 'block';
+        elBotonChats[0].style.top = '200px';
+    }
+
+
+}
